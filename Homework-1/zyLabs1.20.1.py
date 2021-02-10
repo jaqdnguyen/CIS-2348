51d431221a2bae8128e3c9ecd4d1f24ff8f9e50e
Joseph Nguyen <77763346+jaqdnguyen@users.noreply.github.com>
2021-02-09 19:07:36 -0600

    Add files via upload

---
+++ Homework-1/zyLabs1.20.1.py
@@ -0,0 +1,11 @@
+"""Joseph Nguyen PSID: 1779987"""
+# zyLabs 1.20.1 #
+
+user_num = int(input("Enter integer:\n"))
+print("You entered:", user_num)
+print(user_num, "squared is", user_num*user_num)
+print("And", user_num, "cubed is", user_num*user_num*user_num, "!!")
+
+user_num1 = int(input("Enter another integer:\n"))
+print(user_num, "+", user_num1, "is", user_num+user_num1)
+print(user_num, "*", user_num1, "is", user_num*user_num1)
